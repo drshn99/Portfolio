c5f31045ca5376e51687a8a3b42f5a76d03fed6f
drshn99 <57292862+drshn99@users.noreply.github.com>
2023-05-24 08:58:57 -0400

    Portfolio Website

---
+++ script.js
@@ -0,0 +1,46 @@
+// let dot2=document.querySelector('.dot2')
+// let dot1=document.querySelector('.dot1')
+
+// dot1.addEventListener('click',()=>{
+//     document.querySelector('.slider').innerHTML=`
+//     <div class="company">Shraddha (Jan 2022 - Mar 2022)</div>
+//     <div class="role">Software Engineer </div>
+//     <div class="description">
+//       <ul>
+//         <li>Responsible for creating the display page of 
+//           web application using HTML, CSS, and JS.</li>
+//         <li>Tested and debugged the code to ensure its 
+//           quality and performance</li>
+//           <li>Designed a display page that is visually 
+//             appealing as well as mobile friendly.
+            
+//             </li>
+//           <li>Reference: shraddhafabrication.in</li>
+//       </ul>
+//       <div class="changing" style="text-align:center">
+//         <span class="dot" ></span> 
+//         <span class="dot dot2" ></span> 
+        
+//       </div>
+//     </div>
+//     `
+ 
+// })
+
+
+
+
+
+// dot2.addEventListener('click',()=>{
+//     document.querySelector('.slider').innerHTML=`
+//       <div class="changing2" style="text-align:center">
+//       <span class="dot dot1" ></span> 
+//       <span class="dot dot2" ></span> 
+      
+//     </div>
+//     </div>
+//     `
+// })
+
+
+ 
