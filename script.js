4286612c13b08a4bd8d47fcdbe532e6ff486702b
drshn99 <57292862+drshn99@users.noreply.github.com>
2023-05-24 11:45:46 -0400

    Second Push

--- script.js
+++ script.js
@@ -7,40 +7,52 @@
 //     <div class="role">Software Engineer </div>
 //     <div class="description">
 //       <ul>
-//         <li>Responsible for creating the display page of 
+//         <li>Responsible for creating the display page of
 //           web application using HTML, CSS, and JS.</li>
-//         <li>Tested and debugged the code to ensure its 
+//         <li>Tested and debugged the code to ensure its
 //           quality and performance</li>
-//           <li>Designed a display page that is visually 
+//           <li>Designed a display page that is visually
 //             appealing as well as mobile friendly.
-            
+
 //             </li>
 //           <li>Reference: shraddhafabrication.in</li>
 //       </ul>
 //       <div class="changing" style="text-align:center">
-//         <span class="dot" ></span> 
-//         <span class="dot dot2" ></span> 
-        
+//         <span class="dot" ></span>
+//         <span class="dot dot2" ></span>
+
 //       </div>
 //     </div>
 //     `
- 
+
 // })
-
-
-
-
 
 // dot2.addEventListener('click',()=>{
 //     document.querySelector('.slider').innerHTML=`
 //       <div class="changing2" style="text-align:center">
-//       <span class="dot dot1" ></span> 
-//       <span class="dot dot2" ></span> 
-      
+//       <span class="dot dot1" ></span>
+//       <span class="dot dot2" ></span>
+
 //     </div>
 //     </div>
 //     `
 // })
 
-
- 
+const slides = document.querySelectorAll(".slide");
+var counter = 0;
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+const goNext = () => {
+  counter++;
+  slideImage();
+};
+const goPrevious = () => {
+  counter--;
+  slideImage();
+};
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
